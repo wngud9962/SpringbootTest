2ad66a03372267e34f345812578bcca4ca05a676
wngud9962 <jh99620320@gmail.com>
2024-03-13 20:00:44 +0900

    1차파일

---
+++ src/test/java/com/fileupload/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fileupload.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
